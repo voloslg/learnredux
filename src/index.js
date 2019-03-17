108c1d8074c57107fdbc070823f48ec20520edf0
andrey <volosgoto@yahoo.com>
2019-03-17 16:37:06 +0200

    test store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+import { createStore } from 'redux';
+
+function playlist(state = [], action) {
+  if (action.type === 'ADD_TRACK') {
+    return [
+      ...state,
+      action.payload
+    ]
+  }
+  return state;
+};
+
+let store = createStore(playlist);
+// console.log(store.getState());
+
+store.subscribe(() => {
+  console.log('subscribe', store.getState());
+});
+
+store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like teen spirit' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Toxic' });
