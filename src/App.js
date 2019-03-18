bbd9de28825abef04aafd773b4344f0f76b6ff87
volosgoto <volosgoto@yahoo.com>
2019-03-18 15:36:43 +0200

    add track

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react';
-
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class App extends Component {
+  addTrack = () => {
+    console.log("Add track", this.trackInput.value);
+    this.props.onAddTrack(this.trackInput.value);
+    this.trackInput.value = "";
+  };
+
   render() {
+    let { testStore } = this.props;
+    console.log(testStore);
     return (
-      <div className="App">
-        <h1>Test</h1>
+      <div>
+        <input
+          type="text"
+          ref={input => {
+            this.trackInput = input;
+          }}
+        />
+        <button onClick={this.addTrack}>Add track</button>
+        {testStore.map((track, index) => (
+          <li key={index}>{track}</li>
+        ))}
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  dispatch => ({
+    onAddTrack: trackName => {
+      dispatch({ type: "ADD_TRACK", payload: trackName });
+    }
+  })
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,43 +1,46 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-function playlist(store = [], action) {
+const initialState = ["Smells like teen spirit", "Enter sandman"];
+
+function playlist(store = initialState, action) {
   if (action.type === "ADD_TRACK") {
     return [...store, action.payload];
   }
   return store;
 }
 
-let trackInput = document.querySelectorAll(".trackInput")[0];
-let list = document.querySelectorAll(".list")[0];
-
 let store = createStore(playlist);
-store.subscribe(() => {
-  console.log(store.getState());
-  list.innerHTML = "";
-  trackInput.value = "";
-  // console.log("subscribe", store.getState());
-  store.getState().forEach(track => {
-    let li = document.createElement("li");
-    li.innerText = track;
-    list.appendChild(li);
-  });
-});
 
-let addTrackClick = document.querySelectorAll(".addTrack")[0];
-addTrackClick.addEventListener("click", () => {
-  let trackName = trackInput.value;
-  store.dispatch({ type: "ADD_TRACK", payload: trackName });
-});
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+serviceWorker.unregister();
+// let trackInput = document.querySelectorAll(".trackInput")[0];
+// let list = document.querySelectorAll(".list")[0];
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+//   list.innerHTML = "";
+//   trackInput.value = "";
+//   // console.log("subscribe", store.getState());
+//   store.getState().forEach(track => {
+//     let li = document.createElement("li");
+//     li.innerText = track;
+//     list.appendChild(li);
+//   });
+// });
+
+// let addTrackClick = document.querySelectorAll(".addTrack")[0];
+// addTrackClick.addEventListener("click", () => {
+//   let trackName = trackInput.value;
+//   store.dispatch({ type: "ADD_TRACK", payload: trackName });
+// });
